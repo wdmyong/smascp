3438e3269a334bd627fe342106d0f40efbb70a2f
duanyong <duanyong@kuaishou.com>
2018-01-05 22:01:38 +0800

    first commit

---
+++ src/main/java/com/wdm/smascp/SmascpApplication.java
@@ -0,0 +1,12 @@
+package com.wdm.smascp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmascpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmascpApplication.class, args);
+	}
+}
